ab328c66b91951bf7ceefc2b53d64e214eed671d
Felipe <felipe@superhuit.ch>
2023-02-20 09:52:33 +0100

    Track vendor folder

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'superhuit/starterpack-i18n',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6deab17bf5d1ab2ea5e2fd7967a2b4f7c62c98b1',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'superhuit/starterpack-i18n' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6deab17bf5d1ab2ea5e2fd7967a2b4f7c62c98b1',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
